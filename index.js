73830313e71ddde681ad4d71ab16bbedc858069a
van khin <khmaister@yahoo.com>
2024-02-03 20:09:28 -0500

    added package.json + rendering of svg in index.js

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 // requirements
-const inquirer = require('inquirer');
-const fs = require('fs');
-const {Triangle, Circle, Square} = require('./lib/shapes');
+import { Triangle, Circle, Square } from './lib/shapes.js';
+//import { prompt } from "inquirer";
+import { writeFileSync } from 'fs';
+import pkg from 'inquirer';
+const {prompt} = pkg
 
 // Input Prompts
 async function main() {
     try {
-      const answers = await inquirer.prompt([
+      const answers = await prompt([
         {
           type: "input",
           name: "text",
@@ -21,7 +23,7 @@ async function main() {
           type: "list",
           name: "shape",
           message: "Select a shape:",
-          choices: ["Triangle", "Circle", "Square"],
+          choices: ["Triangle", "Circle", "Square"], 
         },
         {
           type: "input",
@@ -29,7 +31,29 @@ async function main() {
           message: "Enter the shape color (keyword or hexadecimal): ",
         },
       ]);
-      const 
+      const { text, textColor, shape, shapeColor } = answers;
+      let logo = `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">`;
+      if (shape === "Triangle") {
+        let triangle = new Triangle();
+        triangle.setColor(shapeColor);
+        logo += triangle.render();
+      } else if (shape === "Circle") {
+        let circle = new Circle();
+        circle.setColor(shapeColor);
+        logo += circle.render();
+      } else if (shape === "Square") {
+        let square = new Square();
+        square.setColor(shapeColor);
+        logo += square.render();
+      }
+      logo += `<text x="60" y="75" font-size="50" text-anchor="middle" fill="${textColor}">${text}</text></svg>`;
 
-    }     
-}
+      writeFileSync('logo.svg', logo);
+      console.log('Generated logo.svg');
+
+    } catch (error) {
+        console.log('an error has occurred', error)
+    };     
+};
+
+main()
